7271cae0b775ec6e226372ce756533f8ec12d7ec
Deepak Kumar Mahanta <162022648+Deepak-Mahanta@users.noreply.github.com>
2025-02-17 20:46:35 +0530

    Corect UserControllerjs

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -17,11 +17,11 @@ const clerkWebhooks = async (req, res) => {
     });
 
     const { data, type } = req.body;
-    switch (key) {
+    switch (type) {
       case "user.created": {
         const userData = {
           clerkId: data.id,
-          email: data.email_addresses[0].email_addresses,
+          email: data.email_addresses[0].email_address,
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
@@ -32,7 +32,7 @@ const clerkWebhooks = async (req, res) => {
       }
       case "user.updated": {
         const userData = {
-          email: data.email_addresses[0].email_addresses,
+          email: data.email_addresses[0].email_address,
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
@@ -42,7 +42,7 @@ const clerkWebhooks = async (req, res) => {
         break;
       }
       case "user.deleted": {
-        await userModel.findOneAndDelete({ clerkId: data.id }, userData);
+        await userModel.findOneAndDelete({ clerkId: data.id });
         res.json({});
         break;
       }
