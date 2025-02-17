d1e82e71a99029665d3431f49bc159e8417ab04e
Deepak Kumar Mahanta <162022648+Deepak-Mahanta@users.noreply.github.com>
2025-02-17 20:05:31 +0530

    new env code added after databace name change

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const useerSchema = new mongoose.Schema({
-clerkId: {type: String, require:true, unique:true},
-email: {type: String, require:true, unique:true},
-photo: { type: String, require:true},
-firstName: {type: String},
-lastName: {type: String},
-creditBalance : {type: Number, default:5}
+  clerkId: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true },
+  photo: { type: String, require: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  creditBalance: { type: Number, default: 5 },
+});
 
+const userModel = mongoose.models.user || mongoose.model("user", useerSchema);
 
-})
-
-const userModel = mongoose.model.user || mongoose.model("user", useerSchema)
-
-export default  userModel;
+export default userModel;
