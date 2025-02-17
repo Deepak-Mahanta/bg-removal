3f179f583a16f267bf48da7f2642496cac3116c8
Deepak Kumar Mahanta <162022648+Deepak-Mahanta@users.noreply.github.com>
2025-02-17 21:03:04 +0530

    database name changed

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/Bg-Removal`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/bgRemoval`);
 };
 
 export default connectDB;
