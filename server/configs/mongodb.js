251c58c8e6ec76bd3ab47950f33e44db9acfcc61
Deepak Kumar Mahanta <162022648+Deepak-Mahanta@users.noreply.github.com>
2025-02-17 20:48:20 +0530

    database name change to capital

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -3,7 +3,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/Bg-Removal`);
 };
 
 export default connectDB;
